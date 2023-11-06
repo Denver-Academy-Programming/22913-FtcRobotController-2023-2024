940b2e4b1c4d1a051d9339afd58acf5a0edfa773
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-11-05 22:06:10 -0700

    More work on the prototype robot script
    More work on the prototype robot script

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeScript.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeScript.java
@@ -8,6 +8,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class PrototypeScript extends LinearOpMode {
+
+    // Define Variable
+    // Motors
+    DcMotor motorFront;
+    DcMotor motorBack;
+    DcMotor motorLeft;
+    DcMotor motorRight;
+
+    // Servo
+    Servo armServo;
+
+    double motorRightPower = gamepad1.right_stick_y;
+    double motorLeftPower = gamepad1.left_stick_y;
     public void runOpMode() {
         // Initialize and setup the modules connected to the Control Hub.
         // Also deliberately sets the right motor in reverse because its
@@ -16,10 +29,10 @@ public class PrototypeScript extends LinearOpMode {
             // All four motors, front, back, left, and right
             // Grab them all from the hardwareMap and set their
             // direction so driving works as intended.
-            DcMotor motorFront = hardwareMap.get(DcMotor.class, "motorNorth");
-            DcMotor motorBack = hardwareMap.get(DcMotor.class, "motorSouth");
-            DcMotor motorLeft = hardwareMap.get(DcMotor.class, "motorEast");
-            DcMotor motorRight = hardwareMap.get(DcMotor.class, "motorWest");
+            motorFront = hardwareMap.get(DcMotor.class, "motorNorth");
+            motorBack = hardwareMap.get(DcMotor.class, "motorSouth");
+            motorLeft = hardwareMap.get(DcMotor.class, "motorEast");
+            motorRight = hardwareMap.get(DcMotor.class, "motorWest");
             motorFront.setDirection(DcMotorSimple.Direction.FORWARD);
             motorBack.setDirection(DcMotorSimple.Direction.REVERSE);
             motorLeft.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -28,9 +41,9 @@ public class PrototypeScript extends LinearOpMode {
             // Servo for the arm
             // Grab it from the hardwareMap and set its range and direction.
             // Also set it to a initial open position.
-            Servo armServo = hardwareMap.get(Servo.class, "armServo");
-            armServo.scaleRange(50, 100);
-            armServo.setPosition(50);
+            armServo = hardwareMap.get(Servo.class, "armServo");
+            armServo.scaleRange(90, 180);
+            armServo.setPosition(90);
         } catch (IllegalArgumentException e) {
             throw new RuntimeException(
                     "An error occurred with the initialization of the script!\n" +
@@ -42,5 +55,12 @@ public class PrototypeScript extends LinearOpMode {
 
         telemetry.addData("Status:", "Modules initialized!");
         telemetry.update();
+
+        while (opModeIsActive()) {
+            // Pressing B on the controller will stop the TeleOP script.
+            if (gamepad1.b) {
+                requestOpModeStop();
+            }
+        }
     }
 }

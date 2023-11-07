b4f6b9ca80c01b312ced794580849e5a98831944
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-11-07 15:59:11 -0700

    Fully working prototype script
    Fully working prototype script

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeScript.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeScript.java
@@ -30,47 +30,51 @@ public class PrototypeScript extends LinearOpMode {
         // Power for motors bound to the controller left and right sticks.
         // leftRightPower is for the front and back motors for moving left and right.
         // forwardBackPower is for the left and right motors for moving forward and back.
-        double leftRightPower = gamepad1.left_stick_y;
-        double forwardBackPower = gamepad1.right_stick_y;
+        double leftRightPower;
+        double forwardBackPower;
 
         // Initialize and setup the modules connected to the Control Hub.
-        // Also deliberately sets the right motor in reverse because its
+        // Also deliberately sets the right motor  in reverse because its
         // physically placed in a reverse position to the left motor.
-        try {
-            // All four motors, front, back, left, and right.
-            // Grab them all from the hardwareMap and set their
-            // direction so driving works as intended.
-            motorFront = hardwareMap.get(DcMotor.class, "motorFront");
-            motorBack = hardwareMap.get(DcMotor.class, "motorBack");
-            motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-            motorRight = hardwareMap.get(DcMotor.class, "motorRight");
-            motorFront.setDirection(DcMotorSimple.Direction.FORWARD);
-            motorBack.setDirection(DcMotorSimple.Direction.REVERSE);
-            motorLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-            motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        //try {
+        // All four motors, front, back, left, and right.
+        // Grab them all from the hardwareMap and set their
+        // direction so driving works as intended.
+        motorFront = hardwareMap.get(DcMotor.class, "motorFront");
+        motorBack = hardwareMap.get(DcMotor.class, "motorBack");
+        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+        motorFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
-            // Servo for the arm.
-            // Grab it from the hardwareMap and set its range and direction.
-            // Also set it to a initial open position.
-            armServo = hardwareMap.get(Servo.class, "armServo");
-            armServo.scaleRange(90, 180);
-            armServo.setPosition(90);
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException(
-                "An error occurred with the initialization of the script!\n" +
-                "Please check you have the right control scheme set on the\n" +
-                "Driver Hub so it knows which modules are connected.\n" +
-                "Full traceback:\n" + e
-            );
-        }
-
+        // Servo for the arm.
+        // Grab it from the hardwareMap and set its range and direction.
+        // Also set it to a initial open position.
+        armServo = hardwareMap.get(Servo.class, "armServo");
+        armServo.scaleRange(0.0, 0.5);
+        armServo.setPosition(0.0);
+//        } catch (IllegalArgumentException e) {
+//            throw new RuntimeException(
+//                "An error occurred with the initialization of the script!\n" +
+//                "Please check you have the right control scheme set on the\n" +
+//                "Driver Hub so it knows which modules are connected.\n" +
+//                "Full traceback:\n" + e
+//            );
+//        }
         telemetry.addData("Status:", "Modules initialized!");
         telemetry.addData("User Action:", "Waiting for user to start script...");
         telemetry.update();
         waitForStart();
 
         while (opModeIsActive()) {
+            // Controller inputs
+            leftRightPower = gamepad1.left_stick_x;
+            forwardBackPower = gamepad1.right_stick_y;
+
             // Display on the Driver Hub info about our robot while its running
+            telemetry.addData("Servo:", armServo.getPosition());
             telemetry.addData("Run Time:", runtime.toString());
             telemetry.addData("Status:", "Script is running!");
             telemetry.addData("leftRightPower:", leftRightPower);
@@ -86,12 +90,14 @@ public class PrototypeScript extends LinearOpMode {
 
             // Pressing X will open the servo arm.
             if (gamepad1.x && !armServoOpen) {
-                armServo.setPosition(90);
+                armServo.setPosition(0.0);
+                armServoOpen = true;
             }
 
             // Pressing Y will close the servo arm.
             if (gamepad1.y && armServoOpen) {
-                armServo.setPosition(180);
+                armServo.setPosition(0.5);
+                armServoOpen = false;
             }
 
             // Pressing B on the controller will stop the TeleOP script.

0cf8ee424ecbdd3c82ada6ffd40bbd998d7f78db
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-11-27 16:31:52 -0700

    Optimized Autonomous scripts
    Because of the API/Library scripts I've created, I've been able to remove many lines of code out of the Autonomous scripts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeV1AutoBlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeV1AutoBlueLeft.java
@@ -6,48 +6,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.library.BasicMoveOperations;
+import org.firstinspires.ftc.teamcode.library.HardwareConfig;
+
 @Autonomous(name = "Prototype V1 Auto Blue Left", group="PrototypeV1")
 public class PrototypeV1AutoBlueLeft extends LinearOpMode {
-    /* Declare OpMode members.*/
-    private DcMotor motorFront = null;
-    private DcMotor motorBack = null;
-    private DcMotor motorLeft = null;
-    private DcMotor motorRight = null;
-    private final ElapsedTime runtime = new ElapsedTime();
-
-    float rightLeftPower = 0.5f;
-    float forwardBackPower = 0.5f;
-    float turningPower = 0.5f;
+    HardwareConfig hardware = new HardwareConfig(this);
 
     @Override
     public void runOpMode() {
-
-        //Initialize and setup the modules that are linked to the control hub.
-        //Setting the motors up so that the robot can function
-        //Setting the left motor to reverse due to its position
-        //relative to the right motors placement so that it can move forward.
-        try {
-            //All four motors front, back, left, and right.
-            //Grab them all from the hardwareMap and set their
-            //direction so that the driving works correctly.
-            motorFront = hardwareMap.get(DcMotor.class, "motorFront");
-            motorBack = hardwareMap.get(DcMotor.class, "motorBack");
-            motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-            motorRight = hardwareMap.get(DcMotor.class, "motorRight");
-
-            motorFront.setDirection(DcMotorSimple.Direction.REVERSE);
-            motorBack.setDirection(DcMotorSimple.Direction.FORWARD);
-            motorRight.setDirection(DcMotorSimple.Direction.FORWARD);
-            motorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        }   catch(IllegalArgumentException e) {
-            throw new RuntimeException(
-                    "A error occurred in the initialization of the script  \n" +
-                    "Please check you have the right motor control scheme set on the \n" +
-                    "Driver Hub so it knows which modules are connected. \n" +
-                    "Full traceback: \n" + e
-
-            );
-        }
+        hardware.init();
 
         telemetry.addData("Status", "Hot Stand by");
         telemetry.addData("Awaiting User Action", "Waiting for user to begin");
@@ -56,55 +24,8 @@ public class PrototypeV1AutoBlueLeft extends LinearOpMode {
         //Waiting for program to begin and user to start the application.
         waitForStart();
 
-        moveForward(1);
-        motorStop();
-        turnLeft(0.35F);
-        motorStop();
-        moveForward(1.30F);
-        motorStop();
-    }
-    // Time 0.35F = About 90 degree turn
-    private void turnLeft(float time){
-        runtime.reset();
-        telemetry.addData("Turning Right Time", time);
-        telemetry.update();
-
-        while (opModeIsActive() && (runtime.seconds() < time)){
-            motorFront.setPower(-turningPower);
-            motorBack.setPower(turningPower);
-            motorLeft.setPower(-turningPower);
-            motorRight.setPower(turningPower);
-        }
-    }
-    // Time 0.35F = About 90 degree turn
-    private void turnRight(float time){
-        runtime.reset();
-        telemetry.addData("Turning Right Time", time);
-        telemetry.update();
-
-        while (opModeIsActive() && (runtime.seconds() < time)){
-            motorFront.setPower(turningPower);
-            motorBack.setPower(-turningPower);
-            motorLeft.setPower(turningPower);
-            motorRight.setPower(-turningPower);
-        }
-    }
-
-    private void moveForward(float time){
-        runtime.reset();
-        telemetry.addData("Moving Forward Time", time);
-        telemetry.update();
-
-        while (opModeIsActive() && (runtime.seconds() < time)){
-            motorLeft.setPower(forwardBackPower);
-            motorRight.setPower(forwardBackPower);
-        }
-    }
-
-    private void motorStop(){
-        motorFront.setPower(0);
-        motorBack.setPower(0);
-        motorLeft.setPower(0);
-        motorRight.setPower(0);
+        BasicMoveOperations.moveForward(0.5F);
+        BasicMoveOperations.turnLeft(0.175F);
+        BasicMoveOperations.moveForward(0.65F);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeV1AutoRedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeV1AutoRedRight.java
@@ -2,17 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.library.BasicMoveOperations;
-import org.firstinspires.ftc.teamcode.library.GVars;
 import org.firstinspires.ftc.teamcode.library.HardwareConfig;
 
 @Autonomous(name = "Prototype V1 Auto Red Right", group="PrototypeV1")
 public class PrototypeV1AutoRedRight extends LinearOpMode {
     HardwareConfig hardware = new HardwareConfig(this);
-
-    private final ElapsedTime runtime = GVars.scriptRunTime;
 
     @Override
     public void runOpMode() {

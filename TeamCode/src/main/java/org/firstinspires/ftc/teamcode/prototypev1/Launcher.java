ccff5fffd1015f51dd68093a8522753c98b98cb3
Joshp9711 <133815654+Joshp9711@users.noreply.github.com>
2023-11-29 10:30:40 -0700

    Update Launcher.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototypev1/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototypev1/Launcher.java
@@ -11,29 +11,34 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 // Here we are telling the driver hub this is tele-operated (i.e., driver controlled) operation mode
 @TeleOp(name="Paper Plane Launcher", group = "Prototype V1 Scripts")
 public class Launcher extends LinearOpMode {
     @Override
     public void runOpMode() {
-        // Define the variables for our hardware
-        Servo servoTest = hardwareMap.get(Servo.class, "servoTest");
-        servoTest.scaleRange(0.0, 0.5);
-        servoTest.setPosition(0.0);
+        
+        Servo ArmServo = hardwareMap.get(Servo.class, "ArmServo");
+        ArmServo.scaleRange(0, 1.0);
+        ArmServo.setPosition(0.0);
 
-        // Queue some data to display on our telemetry screen on the Driver Hub
         telemetry.addData("Status", "Initialized");
-        // Update the telemetry screen on the Driver Hub our queued data
+
         telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
+
         waitForStart();
 
-        // Continuously run until the end of the match (driver presses STOP)
+
         while (opModeIsActive()) {
-            // Update the telemetry screen that the game is running
             telemetry.addData("Status", "Running");
             telemetry.update();
-            servoTest.setPosition(0.5);
+            ArmServo.setPosition(0.0);  }
+
+
+        if (gamepad1.x) {
+            ArmServo.setPosition(0.5);
         }
+
     }
 }
